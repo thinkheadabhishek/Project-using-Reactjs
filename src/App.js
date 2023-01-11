a87ab764ac7dc7edf9abc876591d0bce92037428
thinkheadabhishek <abhishek.borse@thinkitive.com>
2023-01-11 12:24:06 +0530

    Event management project using Reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes , Route}  from 'react-router-dom';
+import Home from './pages/Home';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupsPage from './pages/NewMeetups';
+import FavouritesPage from './pages/Favourites';
+import MainNavigation from './components/layout/MainNavigation';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>  
+    <MainNavigation />
+    <Routes>
+        <Route exact path="/"       element={<Home />} />
+
+        <Route exact path="/all"    element={<AllMeetupsPage />} />
+      
+        <Route exact path="/new"    element={<NewMeetupsPage />}  />
+      
+        <Route exact path="/fav"    element={<FavouritesPage />} />
+    </Routes>
     </div>
   );
 }
